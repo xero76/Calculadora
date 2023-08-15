d5f6bae74fedbc1776eb8fe141e22fffacd3bfb4
Moises Rivera <xero76@gmail.com>
2023-08-14 23:26:40 -0600

    Initial commit

---
+++ Calculadora/Clases/Operaciones.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora.Clases
+{
+    internal class Operaciones
+    {
+        public double Sumar(Double ValorA, Double ValorB)
+        {
+            Double suma = 0;
+            suma = ValorA + ValorB;
+            return suma;
+        }
+
+    }
+}
---
+++ Calculadora/Form1.cs
@@ -0,0 +1,47 @@
+using Calculadora.Clases;
+
+namespace Calculadora
+{
+    public partial class Calculadora : Form
+    {
+        public Calculadora()
+        {
+            InitializeComponent();
+        }
+
+
+        private void brnSuma_Click(object sender, EventArgs e)
+        {
+            Operaciones operaciones = new Operaciones();
+
+            txtResultado.Text =  operaciones.Sumar( double.Parse(txtValorA.Text), double.Parse(txtValorB.Text)).ToString();
+
+        }
+
+        private void btnResta_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMultiplicacion_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDivision_Click(object sender, EventArgs e)
+        {
+
+
+        }
+        private void btnExponente_Click(object sender, EventArgs e)
+        {
+            //      Math.Pow(VariableA ,VariableB)
+
+
+        }
+        private void btnRaiz_Click(object sender, EventArgs e)
+        {
+            //      Math.Pow(VariableB ,(1/VariableA ))  Restriccion: B > 0 
+        }
+    }
+}
